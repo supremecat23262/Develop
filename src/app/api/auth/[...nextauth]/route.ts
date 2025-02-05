fc72015c22f918081cf95e323e7c9ee7724a7956
supremecat23262 <mariorm.md22@universidadupp.edu.mx>
2025-02-05 10:50:22 -0600

    Resolution of conflicts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,22 +1,35 @@
 import NextAuth from "next-auth";
-import { AuthOptions } from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
 
-export const authOptions: AuthOptions = {
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_AUTH_CLIENT_ID ?? "",
-            clientSecret: process.env.GOOGLE_AUTH_CLIENT_SECRET ?? "",
-        }),
-    ],
-    callbacks: {
-        async redirect({ baseUrl }) {
-            return `${baseUrl}/dashboard`;
+// Exportando como GET y POST para manejar las solicitudes HTTP correctamente
+export const GET = (req, res) => {
+    return NextAuth(req, res, {
+        providers: [
+            GoogleProvider({
+                clientId: process.env.GOOGLE_AUTH_CLIENT_ID ?? "",
+                clientSecret: process.env.GOOGLE_AUTH_CLIENT_SECRET ?? "",
+            }),
+        ],
+        callbacks: {
+            async redirect({ baseUrl }) {
+                return `${baseUrl}/dashboard`;
+            },
         },
-    },
+    });
 };
 
-// Exportando como GET y POST para manejar las solicitudes HTTP correctamente
-const handler = (req, res) => NextAuth(req, res, authOptions);
-
-export { handler as GET, handler as POST };
+export const POST = (req, res) => {
+    return NextAuth(req, res, {
+        providers: [
+            GoogleProvider({
+                clientId: process.env.GOOGLE_AUTH_CLIENT_ID ?? "",
+                clientSecret: process.env.GOOGLE_AUTH_CLIENT_SECRET ?? "",
+            }),
+        ],
+        callbacks: {
+            async redirect({ baseUrl }) {
+                return `${baseUrl}/dashboard`;
+            },
+        },
+    });
+};
