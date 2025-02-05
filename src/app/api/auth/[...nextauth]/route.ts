0ba5493ca4a2ed7361fdaa8298d8201d37e8b22d
supremecat23262 <mariorm.md22@universidadupp.edu.mx>
2025-02-05 10:54:24 -0600

    Final commit

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,9 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { NextApiRequest, NextApiResponse } from "next";  // Importa los tipos de Next.js
 
 // Exportando como GET y POST para manejar las solicitudes HTTP correctamente
-export const GET = (req, res) => {
+export const GET = (req: NextApiRequest, res: NextApiResponse) => {
     return NextAuth(req, res, {
         providers: [
             GoogleProvider({
@@ -18,7 +19,7 @@ export const GET = (req, res) => {
     });
 };
 
-export const POST = (req, res) => {
+export const POST = (req: NextApiRequest, res: NextApiResponse) => {
     return NextAuth(req, res, {
         providers: [
             GoogleProvider({
